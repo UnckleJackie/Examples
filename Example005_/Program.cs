2be3eed4fc0a34144a7b274bdd016477be7fb6d0
Puchkov Eugene <ivc037@gmail.com>
2023-09-16 18:02:21 +0300

    add 005 Examplw If Else

---
+++ Example005_/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ипать-стрелять, это же МАША!!!");
+}
+else
+{
+    Console.Write("Ну йопт, привет, мазафака ");
+    Console.WriteLine(username);
+}
